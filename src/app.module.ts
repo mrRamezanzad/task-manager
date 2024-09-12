f24a7936c716e45f7cc72df02c10df57983416ab
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-09-12 17:33:30 +0330

    feat(task): add task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
