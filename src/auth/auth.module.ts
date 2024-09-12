a7681a1e5d372b2e048c9f0e6d5cd88358fe7dfb
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-09-12 17:22:31 +0330

    feat(auth): add passport jwt strategy and auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,22 @@
 import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { UserService } from './user/user.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt/jwt.strategy';
 import { ConfigService } from '@nestjs/config';
 import { jwtConfig } from 'src/config/jwt.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
 
 @Global()
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({ inject: [ConfigService], useFactory: jwtConfig }),
+    PassportModule,
   ],
+  providers: [UserService, JwtStrategy],
+  exports: [UserService]
 })
 export class AuthModule {
 }
