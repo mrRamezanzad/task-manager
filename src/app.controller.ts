9b1d0d9e1da6bf91fdef14a473e2ca2e0a121ad5
mrRamezanzad <mohammadrezaramezanzad@gmail.com>
2024-09-12 17:12:55 +0330

    feat(app): add swagger ApiTags to health check api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService, HealthCheckResponse } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('health')
+@ApiTags('HealthCheck')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
